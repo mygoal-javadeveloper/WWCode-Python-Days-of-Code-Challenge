647ebd998f4edca9311d4b7a9aae8db6e5609418
Nisrin Dhoondia <38102371+mygoal-javadeveloper@users.noreply.github.com>
2024-03-05 21:18:34 +0530

    Add files via upload

--- String/reverseeachword_Day55.py
+++ String/reverseeachword_Day55.py
@@ -1,7 +1,7 @@
 # Create a function that takes a string and returns the reverse of each word
 
 def reverse_each_words_in_string(mystr: str) -> str:
-    return mystr[::-1]
+    return ' '.join([word[::-1] for word in mystr.split()])
 
 
 mystr = input('Enter a sentence: \n').strip()
