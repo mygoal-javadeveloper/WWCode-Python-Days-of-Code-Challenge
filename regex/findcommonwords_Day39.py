449940b6a8adc1f71ed548406f045e81d2218337
Nisrin Dhoondia <38102371+mygoal-javadeveloper@users.noreply.github.com>
2024-03-06 19:59:15 +0530

    Add files via upload

---
+++ regex/findcommonwords_Day39.py
@@ -0,0 +1,15 @@
+# Write a program to find the most common words in a text file
+
+import re
+from collections import Counter
+def findmostcommonwords():
+    text = open('textfile.txt', 'r').read().lower().strip()
+    # text =  open('anothertextfiles.txt', 'r', encoding='utf-8').read().lower().strip()
+    # cleantxt = re.sub(r'\W+', ' ', text).lower().strip().split(' ')
+    # cleantxt = re.sub(r'[^\w\s]', '', str)
+    cleantxt = re.findall(r'[a-z]+', text)
+    mostfreqwords = Counter(cleantxt).most_common(5)
+    print(mostfreqwords)
+
+
+findmostcommonwords()
