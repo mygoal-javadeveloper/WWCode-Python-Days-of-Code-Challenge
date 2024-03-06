afbec31e02c1273b1a4ccf3b68532bc85fbecc4b
Nisrin Dhoondia <38102371+mygoal-javadeveloper@users.noreply.github.com>
2024-03-06 08:36:51 +0530

    Add files via upload

---
+++ regex/regex_url_Day56.py
@@ -0,0 +1,14 @@
+# Create a function to extract all URLs from a given text using regular expressions
+
+import re
+
+def extracturl(mystr: str) -> list:
+    url_pattern = r"(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?([a-zA-Z0-9]{2,})(\.[a-zA-Z0-9]{2,})(\.[a-zA-Z0-9]{2,})?"
+    url_lst = re.findall(url_pattern, mystr)
+    for idx, url in enumerate(url_lst):
+        url_lst[idx] = ''.join(url)
+    return url_lst
+
+
+mystr = input('Enter a sentence containing websitr links: \n').strip()
+print(extracturl(mystr))
