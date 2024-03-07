217a14993870a3d463520595b8acdbae855e7754
Nisrin Dhoondia <38102371+mygoal-javadeveloper@users.noreply.github.com>
2024-03-07 06:43:08 +0530

    Add files via upload

---
+++ Dictionary/findmaxvalkey_Day57.py
@@ -0,0 +1,14 @@
+# Create a function that returns the key with the maximum value in a dictionary
+
+def findmaxvalkey(mydict:dict):
+    return [key for key, val in mydict.items() if val == max(mydict.values())][0]
+
+def findmaxvalkey_sorted(mydict:dict):
+    mydict = sorted(mydict.items(), key=lambda x:x[1], reverse = True)
+    return mydict[0][0]
+
+
+
+mydict = {'A': 5, 'B':1, 'C':121, 'D':10, 'E':21}
+print('The key with max value in the given dictionary is:', findmaxvalkey(mydict))
+print('The key with max value in the given dictionary is:', findmaxvalkey_sorted(mydict))
