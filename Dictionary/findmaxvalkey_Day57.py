f4cc201f9c6d975a8359944788987d8ec4d128ac
Nisrin Dhoondia <38102371+mygoal-javadeveloper@users.noreply.github.com>
2024-03-07 12:24:50 +0530

    Add files via upload

--- Dictionary/findmaxvalkey_Day57.py
+++ Dictionary/findmaxvalkey_Day57.py
@@ -7,8 +7,11 @@ def findmaxvalkey_sorted(mydict:dict):
     mydict = sorted(mydict.items(), key=lambda x:x[1], reverse = True)
     return mydict[0][0]
 
+def findmaxvalkey_max(mydict:dict):
+    return max(mydict, key=mydict.get)
 
 
 mydict = {'A': 5, 'B':1, 'C':121, 'D':10, 'E':21}
 print('The key with max value in the given dictionary is:', findmaxvalkey(mydict))
 print('The key with max value in the given dictionary is:', findmaxvalkey_sorted(mydict))
+print('The key with max value in the given dictionary is:', findmaxvalkey_max(mydict))
